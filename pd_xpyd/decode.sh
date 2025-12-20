6cda561e01e693b0c377448e0622fa2aaa74dff8
Tony Lin <tony.lin@intel.com>
2025-12-20 06:14:42 +0000

    decode script change

--- pd_xpyd/decode.sh
+++ pd_xpyd/decode.sh
@@ -1,4 +1,8 @@
-#bash disaggregated_prefill_server_launcher.sh -n 8 -d 16 --node-ip 192.168.100.221 --node-size 2 --node-rank 0 --dp-master-ip 10.239.129.81 --max-model-len 16384 --max-num-batched-tokens 16384 --max-num-seqs 2 -r decode --async --nixl-buffer-device hpu --warmup
+echo "--node-ip is $1"
+echo "--dp-master-ip is $2"
+echo "--node-rank is $3"
+
+bash disaggregated_prefill_server_launcher.sh -n 8 -d 16 --node-ip $1 --node-size 2 --node-rank $3 --dp-master-ip $2 --max-model-len 16384 --max-num-batched-tokens 16384 --max-num-seqs 32 -r decode --async --nixl-buffer-device hpu --log-dir /workspace $4
 #bash /mnt/disk3/xinyu/ml.infra.tools/scripts/nixl_connector/disaggregated_prefill_server_launcher -m /mnt/disk2/hf_models/DeepSeek-R1-G2-static/ --node-size 2 --node-rank 0 --dp-master-ip 10.239.129.81 -d 16 -n 8 --node-ip 192.168.100.221 --max-model-len 16384 --max-num-seqs 64 -r decode --async --nixl-buffer-device hpu
 #bash disaggregated_prefill_server_launcher.sh -m /mnt/disk2/hf_models/DeepSeek-R1-G2-static/ --node-size 2 --node-rank 0 --dp-master-ip 10.239.129.81 -d 16 -n 8 --node-ip 192.168.100.221 --max-model-len 16384 --max-num-seqs 64 -r decode --async --nixl-buffer-device hpu
-bash decode.sh 192.168.100.221 10.239.129.81 0 
+
