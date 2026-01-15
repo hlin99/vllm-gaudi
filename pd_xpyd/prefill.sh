98eaceffbaf55711e6bddb39c05f16775ec7e0a0
Tony Lin <tony.lin@intel.com>
2026-01-15 05:40:36 +0000

    r1 cmp mismatch

--- pd_xpyd/prefill.sh
+++ pd_xpyd/prefill.sh
@@ -3,7 +3,7 @@ BASH_DIR=$(dirname "${BASH_SOURCE[0]}")
 # 16K model len for prefill
 #bash "$BASH_DIR"/disaggregated_prefill_server_launcher.sh -m /mnt/disk2/hf_models/DeepSeek-R1-G2/ -n 1 -t 8 --node-ip 192.168.100.191 --max-model-len 8192 --max-num-batched-tokens 8192 --max-num-seqs 8 --nixl-buffer-device hpu --log-dir /workspace --gpu-memory-utilization 0.35 --inc /workspace/vllm-gaudi/pd_xpyd/inc_ep8/maxabs_quant_g2_ep8.json --chunked-prefill --kv-connector lmcache 
 
-bash "$BASH_DIR"/disaggregated_prefill_server_launcher.sh -m /mnt/disk2/hf_models/DeepSeek-V2-Lite/ -n 1 -t 8 --node-ip 192.168.100.191 --max-model-len 8192 --max-num-batched-tokens 8192 --max-num-seqs 8 --nixl-buffer-device hpu --log-dir /workspace --gpu-memory-utilization 0.35 --kv-connector lmcache
+bash "$BASH_DIR"/disaggregated_prefill_server_launcher.sh -m /mnt/disk2/hf_models/DeepSeek-R1-G2/ -n 1 -t 8 --node-ip 192.168.100.191 --max-model-len 8192 --max-num-batched-tokens 8192 --max-num-seqs 8 --nixl-buffer-device hpu --log-dir /workspace --gpu-memory-utilization 0.35 --kv-connector lmcache --inc /workspace/vllm-gaudi/pd_xpyd/inc_ep8/maxabs_quant_g2_ep8.json
 
 # 128K model len for prefill
 # bash "$BASH_DIR"/disaggregated_prefill_server_launcher.sh -m /mnt/disk2/hf_models/DeepSeek-R1-G2/ -n 1 -t 8 --node-ip 192.168.100.191 --max-model-len 131072 --max-num-batched-tokens 8192 --max-num-seqs 1 --nixl-buffer-device hpu --log-dir /workspace --gpu-memory-utilization 0.711 --inc /workspace/vllm-gaudi/pd_xpyd/inc_ep8/maxabs_quant_g2_ep8.json --apc --warmup
