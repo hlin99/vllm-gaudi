430be32c4f80cdc9807853179a852a35cb5f6a89
Tony Lin <tony.lin@intel.com>
2026-04-17 07:34:30 +0000

    2p1d works
    Signed-off-by: Tony Lin <tony.lin@intel.com>

--- pd_xpyd/16P8D/proxy.sh
+++ pd_xpyd/16P8D/proxy.sh
@@ -4,6 +4,6 @@ BASH_DIR=$(dirname "${BASH_SOURCE[0]}")
 python "$BASH_DIR"/../proxy_server.py \
   --port 8868 \
   -m /mnt/disk2/hf_models/DeepSeek-V2-Lite-Chat/ \
-  -p 192.168.100.191:8300 \
+  -p 192.168.100.191:8300 192.168.100.221:8300 \
   -d 192.168.100.231:9300 \
   --bypass-proxy \
